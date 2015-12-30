15dbc487ddd5257c9eca5c9e089a3f03b08dd9dc
David Asfaha <davidasfaha@gmail.com>
2015-12-30 16:41:27 +0000

    Implemented half of all ArrayListMethods

--- ArrayListImpl.java
+++ ArrayListImpl.java
@@ -3,10 +3,12 @@ class ArrayListImpl implements List{
 	private Object[] internalArray;
 	private int internalArrayCount;
 	public static final int ARRAY_SIZE_START  = 1000000;
-	
+	private int internalArrayCapacity;
+    
 	public ArrayListImpl(){
 		this.internalArray = new Object[this.ARRAY_SIZE_START];
 		this.internalArrayCount = 0;
+        this.internalArrayCapacity = this.ARRAY_SIZE_START;
 	}
 
     public boolean isEmpty(){
@@ -22,7 +24,12 @@ class ArrayListImpl implements List{
 
     
     public ReturnObject get(int index){
-        ReturnObjectImpl roi = new ReturnObjectImpl("I'M A DUMMY - IMPLEMENT ME!");
+        if (index < 0 || index > this.internalArrayCapacity ){
+            ReturnObjectImpl roi = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+            return roi;
+        }
+
+        ReturnObjectImpl roi = new ReturnObjectImpl(this.internalArray[index]);
         return roi;
     }
 
@@ -36,10 +43,29 @@ class ArrayListImpl implements List{
         return roi;
     }
 
-   public ReturnObject add(Object item){
-        ReturnObjectImpl roi = new ReturnObjectImpl("I'M A DUMMY - IMPLEMENT ME!");
-        return roi;
-   } 
+   public ReturnObjectImpl add(Object item){
+        if (item == null) {
+            ReturnObjectImpl roi = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+            return roi;
+        }else{
+           if(this.internalArrayCapacity==this.internalArrayCount){
+                this.internalArrayCapacity *= 2;
+                Object[] newInternalArray = new Object[this.internalArrayCapacity];
+                for(int i =0; i<=this.internalArray.length-1; i++){
+                    newInternalArray[i] = this.internalArray[i];
+                }     
+                this.internalArray = newInternalArray;
+           }
+           this.internalArray[this.internalArrayCount] = item; 
+           this.internalArrayCount+=1;
+            
+        } 
+        return null;
+   }
+   
+   private void doubleCapacity(){
+    // Double the capacity of the array
+   }
 }    
 	
 	
--- TestArrayListImpl.java
+++ TestArrayListImpl.java
@@ -7,6 +7,30 @@ class TestArrayListImpl {
         System.out.println("Finished tests for TestArrayListImpl");
         if(testArray.size()!=0)
             System.out.println("TestArrayListImpl-Failed: size()!=0 after object creation");
+        testArray.add("Hello,");
+        testArray.add("is it me");
+        testArray.add("you're looking for");
+        if(testArray.isEmpty())
+            System.out.println("TestArrayListImpl-Failed: isEmpty() on non empty list");
+        if(testArray.size()!=3)
+            System.out.println("TestArrayListImpl-Failed: size()!=3 when 3 elements are in list");
+        ReturnObjectImpl errRetObj = testArray.add(null);
+        if(errRetObj.getError()!=ErrorMessage.INVALID_ARGUMENT)
+            System.out.println("TestArrayListImpl-Failed: getError()!=ErrorMessage.INVALID_ARGUMENT when invalid argument was supplied");
+        ReturnObjectImpl nullRetObj = testArray.add("Lionel Richie");
+        if(nullRetObj!=null)
+            System.out.println("TestArrayListImpl-Failed: nullRetObj!=null  when valid argument was supplied");
+        int counter = 1000000-4;
+        //fill array up to initial capacity
+        while (counter>0){
+            testArray.add("Element:" + counter);
+            counter-= 1;
+        }
+        testArray.add("Will I fit?");
+        if(testArray.size()!= 1000001)
+            System.out.println("TestArrayListImpl-Failed: size()!=1000001 after adding element that expands list beyond default capacity");
+        System.out.println("Number of elements in array are: " + testArray.size());
+
     }
 
 }
