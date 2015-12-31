c6fcbbc1db75180c23489f05b6f5e1309ce9bd67
David Asfaha <davidasfaha@gmail.com>
2015-12-31 20:46:29 +0000

    Implemented linkedlist to add object to end and get element

--- TestListImpl.java
+++ TestListImpl.java
@@ -19,8 +19,8 @@ class TestListImpl {
         ReturnObjectImpl nullRetObj = testArray.add("Lionel Richie");
         if(nullRetObj!=null)
             System.out.println("TestArrayListImpl-Failed: nullRetObj!=null  when valid argument was supplied");
-        //int counter = 1000000-4;
-        int counter = 10-4;
+        int counter = 1000000-4;
+        //int counter = 10-4;
         //fill array up to initial capacity
         while (counter>0){
             testArray.add("Element:" + counter);
@@ -36,20 +36,18 @@ class TestListImpl {
         */
         if(testArray.size()!= 1000001)
             System.out.println("TestArrayListImpl-Failed: size()!=1000001 after adding element that expands list beyond default capacity");
-        System.out.println("Number of elements in array are: " + testArray.size());
-        System.out.println("---->" + testArray.get(0).getReturnValue());
-        System.out.println("---->" + testArray.get(1).getReturnValue());
-        System.out.println("---->" + testArray.get(2).getReturnValue());
+
         if(!testArray.get(0).getReturnValue().equals("Hello,") || !testArray.get(500000).getReturnValue().equals("Element:500000") || !testArray.get(testArray.size()-1).getReturnValue().equals("Will I fit?"))
+            System.out.println("TestArrayListImpl-Failed: get() not returning first, middle and last elements correctly");
+/*
             System.out.println("First element:" + testArray.get( 0 ).getReturnValue().equals("Hello,"));
             System.out.println("Middle element:" + testArray.get( 500000 ).getReturnValue().equals("Element:500000"));
-            //System.out.println("Last element:" + testArray.get(testArray.size()-1).getReturnValue().equals("Will I fit?"));
+            System.out.println("Last element:" + testArray.get(testArray.size()-1).getReturnValue().equals("Will I fit?"));
                 
             System.out.println("First element:" + testArray.get( 0 ).getReturnValue());
             System.out.println("Middle element:" + testArray.get( 500000 ).getReturnValue());
             System.out.println("Last element:" + testArray.get(testArray.size()-1).getReturnValue());
-
-            System.out.println("TestArrayListImpl-Failed: get() not returning first, middle and last elements correctly");
+*/
         ReturnObjectImpl errRoiOutOfBounds = testArray.get(-1); 
         ReturnObjectImpl errRoiOutOfBounds2 = testArray.get(testArray.size());
         System.out.println("RUNNING HERE");
@@ -77,3 +75,4 @@ class TestListImpl {
         System.out.println("Finished tests for TestArrayListImpl");
     }
 }
+
